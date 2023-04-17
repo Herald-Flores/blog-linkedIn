1d9296d2e084d8daa9e21a7adf22efb498bacffe
Herald-Flores <heraldflores95@gmail.com>
2023-04-16 21:18:38 -0600

    feat: add router and Pages navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/HomePage/HomePage'
+import AboutPage from './pages/AboutPage/AboutPage'
+import ArticlesListPage from './pages/ArticlesListPage/ArticlesListPage'
+import ArticlePage from './pages/ArticlePage/ArticlePage'
+import NavBar from './Components/NavBar/NavBar'
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
 
 function App() {
   return (
-    <div>
-      <h1>Vite + React</h1>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about-us' element={<AboutPage />} />
+        <Route path='/blog' element={<ArticlesListPage />} />
+        <Route path='article/:articleId' element={<ArticlePage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
